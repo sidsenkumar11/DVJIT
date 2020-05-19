c9bd6180c785e0980c6fc6eed539b4802be68640
Siddarth Senthilkumar <sidsenkumar11@gmail.com>
2020-05-19 14:35:00 -0700

    trying to fix travis infinitely building

--- tests/shell.sh
+++ tests/shell.sh
@@ -3,6 +3,6 @@
 cd ../exploits/getshell
 python3 gen_exploit.py
 cd ../../badlang
-given=`echo "echo 'hello from inside'" | ./bin/badlang ../exploits/getshell/exploit.bad`
+given=`echo "echo 'hello from inside';exit" | ./bin/badlang ../exploits/getshell/exploit.bad`
 expected="hello from inside"
 [[ "$given" == "$expected" ]]
